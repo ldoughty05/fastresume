2bff0cc674bbdb7bb9c1fa48064081fd1e84062e
ldoughty2 <ldoughty2@huskers.unl.edu>
2025-03-01 16:06:24 -0600

    added urls to experiences/ and experiences/delete

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Job, Project
+from .models import * 
 
 
 class ExperienceListCreate(generics.ListCreateAPIView):
