090d13937aefed3df0efbf208de16e1b25bf32cb
ldoughty2 <ldoughty2@huskers.unl.edu>
2025-07-19 12:40:14 -0500

    updated CORS to allow authentication from localhost to api.fastresume.org

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
+from corsheaders.defaults import default_headers, default_methods
+
 
 load_dotenv()
 
@@ -61,6 +63,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -68,7 +71,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -152,4 +154,14 @@ CORS_ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:5174"
 ]
-CORS_ALLOWS_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'content-type',
+    'authorization',
+]
+
+CORS_ALLOW_METHODS = list(default_methods) + [
+    'OPTIONS',
+]
+
+CORS_ALLOW_CREDENTIALS = True
