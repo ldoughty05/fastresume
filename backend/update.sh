2e1f6d7040e4a2e7ef472b1e1e67fe34d0714516
Luke Doughty <luke.doughty@me.com>
2025-07-21 19:09:08 +0100

    made update.sh handle when services arent running. Prettified it with emojis

--- backend/update.sh
+++ backend/update.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 echo "💠 Pulling latest changes from main branch..."
 git pull origin main
 
-echo "💠Installing dependencies..."
+echo "💠 Installing dependencies..."
 pip install --upgrade pip --quiet
 pip install -r requirements.txt --quiet
 
@@ -15,14 +15,30 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py check
 
-echo "💠 Restarting FastResume backend..."
-if [ -f gunicorn.pid ]; then
-  sudo kill -HUP $(cat gunicorn.pid) # restart Gunicorn
-else
-  echo "No Gunicorn PID file found, restarting systemd service."
-  sudo systemctl restart fastresumebackend # restart the service
+echo "💠 Checking if services are running..."
+if systemctl is-active --quiet nginx; then
+	echo "✅ nginx is running"
+else 
+	echo "nginx needs to restart"
+	sudo systemctl restart nginx
+	if systemctl is-active --quiet nginx; then
+		echo "✅ nginx restarted successfully and is running"
+    	else
+        	echo "❌ Failed to restart nginx"
+    	fi
 fi
 
-echo `💠 date "+%Y-%m-%d %H:%M:%S.%3N"` ' FastResume updated' >> fastresumebackend_update.log
+if systemctl is-active --quiet fastresumebackend; then
+	echo "✅ fastresumebackend is running"
+else 
+	echo "fastresumebackend needs to restart"
+	sudo systemctl restart fastresumebacked
+	if systemctl is-active --quiet fastresumebackend; then
+		echo "✅ fastresumebackend restarted successfully and is running"
+    	else
+        	echo "❌ Failed to restart fastresumebackend"
+    	fi
+fi
+echo "💠 $(date "+%Y-%m-%d %H:%M:%S.%3N") FastResume updated" >> fastresumebackend_update.log
 popd
 echo "✅ FastResume updated and restarted successfully."
