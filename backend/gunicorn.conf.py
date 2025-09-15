20cc9c6cd0ee377172bb2d51e5c0c039f775b3a5
ldoughty2 <ldoughty2@huskers.unl.edu>
2025-09-15 17:38:47 -0500

    modified gunicorn config to work on VPS instead of pi

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -8,9 +8,9 @@ timeout = 120
 proc_name = "fastresumebackend"
 
 # Access log - records incoming HTTP requests
-accesslog = "/home/pi/Git/fastresume/backend/logs/gunicorn-fastresumebackend.access.log"
+accesslog = "/root/Git/fastresume/backend/logs/gunicorn-fastresumebackend.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "/home/pi/Git/fastresume/backend/logs/gunicorn-fastresumebackend.error.log"
+errorlog = "/root/Git/fastresume/backend/logs/gunicorn-fastresumebackend.error.log"
 # Whether to send Django output to the error log
 capture_output = True
 # How verbose the Gunicorn error logs should be
